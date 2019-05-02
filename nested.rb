9efdf039776e70669ebf5d1e472b43ac26a9b144
Lan Nguyen <nguyenlan13@gmail.com>
2019-05-02 12:34:21 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,7 +85,12 @@ def adding_matz
         }
      }
 
-programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ""
+programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+
+programmer_hash
+
+
+}]
     
 end
 
