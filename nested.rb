61abd2a36c9f08fa58ad134bb14d9b1137e944f9
Lan Nguyen <nguyenlan13@gmail.com>
2019-05-02 12:30:05 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,9 +15,10 @@ def hopper
           :languages => ["C"]
         }
      }
-
-
+programmer_hash[:grace_hopper]
 end
+
+
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
@@ -37,7 +38,10 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+    programmer_hash[:alan_kay][:known_for]
 end
+
 
 def dennis_ritchies_language
 	programmer_hash = 
@@ -55,6 +59,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages].join("")
 end
 
 def adding_matz
@@ -80,6 +85,7 @@ def adding_matz
         }
      }
 
+programmer_hash[:yukihiro_matsumoto]
     
 end
 
@@ -102,6 +108,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
+     programmer_hash[:alan_kay][:known_for] = "GUI"
      
 end
 
@@ -123,3 +130,5 @@ def adding_to_dennis
      }
 
 end
+
+
