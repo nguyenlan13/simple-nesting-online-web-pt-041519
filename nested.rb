c71f9512443f92851c4ab9ada1f2b9ffa04a09e4
Lan Nguyen <nguyenlan13@gmail.com>
2019-05-02 12:31:56 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,7 +85,7 @@ def adding_matz
         }
      }
 
-programmer_hash[:yukihiro_matsumoto]
+programmer_hash[:yukihiro_matsumoto] = [:known_for]
     
 end
 
@@ -106,9 +106,13 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
+     
+     
      alans_new_info = "GUI"
      
-     programmer_hash[:alan_kay][:known_for] = "GUI"
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     
+     programmer_hash
      
 end
 
